4be02e69e29cf021b8046b17fdf9571737de0a90
lxw-peter <1270542959@qq.com>
2020-04-23 00:27:07 +0800

    更新

--- week02/encode.js
+++ week02/encode.js
@@ -5,9 +5,9 @@ function encode(str) {
 console.log(encode('你好啊')) // Uint8Array(9) [228, 189, 160, 229, 165, 189, 229, 149, 138]
 
 function encode2(str) {
-    let result = encodeURIComponent(str).replace(/%/gm, ',\\u0x').split('\\');
+    let result = encodeURIComponent(str).replace(/%/gm, 'u0x').split('u');
     result.shift();
     return result;
 }
 
-console.log(encode2('你好啊')) // ["u0xE4,", "u0xBD,", "u0xA0,", "u0xE5,", "u0xA5,", "u0xBD,", "u0xE5,", "u0x95,", "u0x8A"]
+console.log(encode2('$')) // ["u0xE4,", "u0xBD,", "u0xA0,", "u0xE5,", "u0xA5,", "u0xBD,", "u0xE5,", "u0x95,", "u0x8A"]
